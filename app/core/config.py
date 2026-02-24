ce7473e6b43500dcaedbf8453acc581dc2822390
Caique Feitosa <f.c.caique2000@gmail.com>
2026-02-24 18:42:12 -0300

    add config.py com pydantic settings

---
+++ app/core/config.py
@@ -0,0 +1,33 @@
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+from typing import Optional
+
+class Settings(BaseSettings):
+    APP_NAME: str = "PressStart"
+    APP_ENV: str = "development"
+    SECRET_KEY: str
+
+    DATABASE_URL: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+
+    RAWG_API_KEY: str
+    RAWG_BASE_URL: str = "https://api.rawg.io/api"
+
+    @field_validator("APP_ENV")
+    @classmethod
+    def validate_app_env(cls, v: str) -> str:
+        allowed = ["development", "production", "testing"]
+        if v not in allowed:
+            raise ValueError(f"APP_ENV must be one of{allowed}")
+        return v
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+settings = Settings()
+
+    
